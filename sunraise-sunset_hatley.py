4e4c941da51960c70ed6301cd4931d3b806c8443
Trollpatrol <75658085+Trollpatrol@users.noreply.github.com>
2021-01-19 18:19:07 -0500

    Add files via upload

---
+++ sunraise-sunset_hatley.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  4 15:37:13 2020
+
+@author: JN
+"""
+
+import datetime
+from suntime import Sun, SunTimeException
+
+latitude = 45.307840
+longitude = -71.992140
+
+sun = Sun(latitude, longitude)
+
+# Get today's sunrise and sunset in UTC
+today_sr = sun.get_sunrise_time()
+today_ss = sun.get_sunset_time()
+print('Today at Hatley the sun raised at {} and get down at {}'.
+      format(today_sr.strftime('%H:%M'), today_ss.strftime('%H:%M')))
